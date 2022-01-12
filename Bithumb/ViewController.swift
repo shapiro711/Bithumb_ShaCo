9c7b3859f55f0954d57cfbcadf36bc30907bff80
shpairo711 <shapiro7@ajou.ac.kr>
2022-01-12 22:53:14 +0900

    Initial Commit

---
+++ Bithumb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bithumb
+//
+//  Created by Kim Do hyung on 2022/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
