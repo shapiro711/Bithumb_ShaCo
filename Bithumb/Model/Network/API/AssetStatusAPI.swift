8280e2ec3da846f0c79d8987f793f04d307d2cb6
ictechgy <ictechgy@gmail.com>
2022-01-15 00:03:15 +0900

    ✨ [Feat]: AssetsStatus 요청을 위한 AssetStatusAPI 타입 구현

---
+++ Bithumb/Model/Network/API/AssetStatusAPI.swift
@@ -0,0 +1,39 @@
+//
+//  AssetStatusAPI.swift
+//  Bithumb
+//
+//  Created by JINHONG AN on 2022/01/14.
+//
+
+import Foundation
+
+enum AssetStatusAPI {
+    case lookUp(orderCurrency: String)
+}
+
+extension AssetStatusAPI: Requestable {
+    var apiType: APIType {
+        return .rest
+    }
+    
+    var requestType: RequestType {
+        return .assetStatus
+    }
+    
+    var pathParameters: [String : Any]? {
+        switch self {
+        case .lookUp(let orderCurrency):
+            var params = [String: Any]()
+            params["orderCurrency"] = orderCurrency
+            return params
+        }
+    }
+    
+    var queryParameters: [String : Any]? {
+        return nil
+    }
+    
+    var messageParameters: [String : Any]? {
+        return nil
+    }
+}
