491b356b3e6dcb306e1c53f03fd655740ca674d3
shpairo711 <shapiro7@ajou.ac.kr>
2022-01-18 22:40:48 +0900

    ✨ [Feat]: 입출금현황을 위한 AssetStatusDTO 구현

---
+++ Bithumb/Controller/DTO/AssetStatusDTO.swift
@@ -0,0 +1,18 @@
+//
+//  AssetStatusDTO.swift
+//  Bithumb
+//
+//  Created by Kim Do hyung on 2022/01/18.
+//
+
+import Foundation
+
+struct AssetStatusDTO {
+    let symbol: String
+    let data: AssetStatusData
+    
+    struct AssetStatusData {
+        let depositStatus: Possibility
+        let withdrawalStatus: Possibility
+    }
+}
