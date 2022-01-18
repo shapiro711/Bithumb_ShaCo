6fb0e7a61cfed07fb87e31098245efe02236e0cb
shpairo711 <shapiro7@ajou.ac.kr>
2022-01-18 22:11:56 +0900

    ✨ [Feat]: 호가창에 대한 OrderBookDepthDTO 구현

---
+++ Bithumb/Controller/DTO/OrderBookDTO.swift
@@ -0,0 +1,19 @@
+//
+//  OrderBookDTO.swift
+//  Bithumb
+//
+//  Created by Kim Do hyung on 2022/01/18.
+//
+
+import Foundation
+
+struct OrderBookDepthDTO {
+    let bids: [OrderBookData]
+    let asks: [OrderBookData]
+    
+    struct OrderBookData {
+        let type: OrderType
+        let price: Double
+        let quantity: Double
+    }
+}
