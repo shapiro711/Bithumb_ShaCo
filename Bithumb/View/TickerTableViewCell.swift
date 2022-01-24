fb16ad230e2054f3846fcc196e8c706401ed7ffd
shpairo711 <shapiro7@ajou.ac.kr>
2022-01-24 23:40:23 +0900

    💄 [UI]: TickerTableViewCell이 화면에 정상적으로 보이도록 수정

--- Bithumb/View/TickerTableViewCell.swift
+++ Bithumb/View/TickerTableViewCell.swift
@@ -15,6 +15,8 @@ final class TickerTableViewCell: UITableViewCell {
         tickerInformationStackView.translatesAutoresizingMaskIntoConstraints = false
         tickerInformationStackView.axis = .horizontal
         tickerInformationStackView.alignment = .firstBaseline
+        tickerInformationStackView.distribution = .fill
+        tickerInformationStackView.spacing = 8
         return tickerInformationStackView
     }()
     private let identityStackView: UIStackView = {
@@ -32,39 +34,42 @@ final class TickerTableViewCell: UITableViewCell {
     
     private let nameLabel: UILabel = {
         let nameLabel = UILabel()
-        nameLabel.font = .preferredFont(forTextStyle: .title2)
+        nameLabel.font = .preferredFont(forTextStyle: .body)
+        nameLabel.numberOfLines = 0
         return nameLabel
     }()
     private let symbolLabel: UILabel = {
         let symbolLabel = UILabel()
-        symbolLabel.font = .preferredFont(forTextStyle: .title3)
+        symbolLabel.font = .preferredFont(forTextStyle: .caption1)
         symbolLabel.textColor = .systemGray
         return symbolLabel
     }()
     private let currentPriceLabel: UILabel = {
         let currentPriceLabel = UILabel()
-        currentPriceLabel.font = .preferredFont(forTextStyle: .title2)
+        currentPriceLabel.font = .preferredFont(forTextStyle: .body)
+        currentPriceLabel.textAlignment = .right
         return currentPriceLabel
     }()
     private let fluctuatedRateLabel: UILabel = {
         let fluctuatedRateLabel = UILabel()
-        fluctuatedRateLabel.font = .preferredFont(forTextStyle: .title2)
+        fluctuatedRateLabel.font = .preferredFont(forTextStyle: .body)
         return fluctuatedRateLabel
     }()
     private let fluctuatedPriceLabel: UILabel = {
         let fluctuatedPriceLabel = UILabel()
-        fluctuatedPriceLabel.font = .preferredFont(forTextStyle: .title3)
+        fluctuatedPriceLabel.font = .preferredFont(forTextStyle: .caption1)
         return fluctuatedPriceLabel
     }()
     private let transactionAmountLabel: UILabel = {
         let transactionAmountLabel = UILabel()
-        transactionAmountLabel.font = .preferredFont(forTextStyle: .title2)
+        transactionAmountLabel.font = .preferredFont(forTextStyle: .body)
         return transactionAmountLabel
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         buildHierarchy()
+        laysOutConstranints()
     }
     
     required init?(coder: NSCoder) {
@@ -86,6 +91,20 @@ extension TickerTableViewCell {
         
         fluctuationStackView.addArrangedSubview(fluctuatedRateLabel)
         fluctuationStackView.addArrangedSubview(fluctuatedPriceLabel)
+    }
+    
+    private func laysOutConstranints() {
+        let margin = 8.0
+        NSLayoutConstraint.activate([
+            tickerInformationStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: margin),
+            tickerInformationStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: margin),
+            tickerInformationStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -margin),
+            tickerInformationStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -margin),
+            
+            identityStackView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.25),
+            currentPriceLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.25),
+            fluctuationStackView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.15)
+        ])
     }
     
     func configure(by tickerInformation: TickerDTO) {
